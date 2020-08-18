31e04a9ee79aacfb9ed269ad6e8256b2c9965b8f
Shuvax <sbanik7458@gmail.com>
2020-08-18 17:29:49 -0400

    Updates

---
+++ precache-manifest.a8aed5a2542cee28d11d83e8c1fad82b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "945f6dc8ea84ec144ab300e6d37f5e7f",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "2fbaef5b263a5a500da1",
+    "url": "/react-todolist/static/css/main.0ab9ee0f.chunk.css"
+  },
+  {
+    "revision": "7ab5e20459a70a9e4cdf",
+    "url": "/react-todolist/static/js/2.f826d3d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todolist/static/js/2.f826d3d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fbaef5b263a5a500da1",
+    "url": "/react-todolist/static/js/main.e50803f1.chunk.js"
+  },
+  {
+    "revision": "e0db50d6f38e2b5ad310",
+    "url": "/react-todolist/static/js/runtime-main.24e51d31.js"
+  }
+]);
